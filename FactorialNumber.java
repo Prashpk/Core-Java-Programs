b517a6728f597ae4b7b1f67dd0a7ad9f9197548f
Prashanth <99880212+Prashpk@users.noreply.github.com>
2022-04-15 22:52:47 +0530

    Update and rename FactorialOfaNumber.java to FactorialNumber.java

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -2,21 +2,20 @@ package projects1;
 
 import java.util.Scanner;
 
-public class FactorialOfaNumber {
+public class FactorialNumber {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		int num,factorial=1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number :");
 		num = sc.nextInt();
 		
-	    int i = num;
+	        int i = num;
 		while(num>=1) {
 			 factorial = factorial * num;
 			 num = num-1;
 		}
-		System.out.println("factorial of number " +i+ " is "+factorial);
+		System.out.println("factorial of a number " +i+ " is "+factorial);
 
 	}
 
