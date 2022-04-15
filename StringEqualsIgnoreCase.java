8fcc7f41d3a7e90df31ba15c9e9da1f41202282a
Prashanth <99880212+Prashpk@users.noreply.github.com>
2022-04-16 00:07:48 +0530

    Update and rename Pg_18_String_Equals_Ignorecase.java to StringEqualsIgnoreCase.java

--- StringEqualsIgnoreCase.java
+++ StringEqualsIgnoreCase.java
@@ -1,9 +1,8 @@
 package Projects2;
 import java.util.Scanner;
 
-public class Pg_18_String_Equals_Ignorecase {
+public class StringEqualsIgnoreCase {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		int i;
 		String uname,upass;
