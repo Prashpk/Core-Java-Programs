1156e04cefe0b092bc7669867ab4e6c6809d143d
Prashanth <99880212+Prashpk@users.noreply.github.com>
2022-04-15 23:07:31 +0530

    Update and rename P1_KboatNameInitials.java to KboatNameInitials.java

--- KboatNameInitials.java
+++ KboatNameInitials.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class P1_KboatNameInitials
+public class KboatNameInitials
 {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
