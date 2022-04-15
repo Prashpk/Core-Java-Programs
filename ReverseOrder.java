242f9ba8ae0f0a82b4514079f3701b31fecb1c96
Prashanth <99880212+Prashpk@users.noreply.github.com>
2022-04-15 23:46:18 +0530

    Update and rename Reverse_100_To_1.java to ReverseOrder.java

--- ReverseOrder.java
+++ ReverseOrder.java
@@ -3,7 +3,7 @@
 //program to print 100 to 1 in reverse order of no
 package projects1;
 
-public class Reverse_100_To_1 {
+public class ReverseOrder {
 
 	public static void main(String[] args) {
 			    
