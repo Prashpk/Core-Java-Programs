95c0654882688aa32f32163adfdd18dfed5c17ab
Prashanth <99880212+Prashpk@users.noreply.github.com>
2022-04-15 22:55:24 +0530

    Update LargestTwoNumbers.java

--- LargestTwoNumbers.java
+++ LargestTwoNumbers.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 public class LargestTwoNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		int num1, num2;
-		
-		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number 1");
 		num1 = sc.nextInt();
 		System.out.println("Enter the number 2");
 		num2 = sc.nextInt();
-		
 		
 		if(num1>num2) {
 			System.out.println(+num1+ " is larger than " +num2);
@@ -24,7 +19,5 @@ public class LargestTwoNumbers {
 		else {
 			System.out.println("Two Numbers Are Equal");
 		}
-
 	}
-
 }
