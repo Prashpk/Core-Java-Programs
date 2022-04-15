150a71773d108a778b97975ca253a8e7abe710b3
Prashanth <99880212+Prashpk@users.noreply.github.com>
2022-04-15 23:37:20 +0530

    Update and rename Prime_or_Not.java to PrimeOrNot.java

---
+++ PrimeOrNot.java
@@ -0,0 +1,20 @@
+
+package JavaPrograms1;
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+		int i,num=6;
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("Enter the number to check prime or not ?");
+		//num = sc.nextInt();
+		for(i=0;i<1;i++) {
+			if(num%2==0) {
+				System.out.println(num+ " Is not a Prime no");
+			}
+			else {
+				System.out.println(num+ " is a prime number");
+			}
+		}
+	}
+}
